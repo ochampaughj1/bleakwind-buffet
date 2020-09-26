7700dcb497897624d6d77dcc06943bcdfdd8b9e8
Jonathan Ochampaugh <ochampaughj1@ksu.edu>
2020-09-26 10:17:12 -0500

    Added lots of GUI data

---
+++ POS Milestone 1/Drinks/SailorSoda.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS_Milestone_1.Drinks
+{
+    /// <summary>
+    /// Interaction logic for SailorSoda.xaml
+    /// </summary>
+    public partial class SailorSoda : UserControl
+    {
+        public SailorSoda()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ POS Milestone 1/Entrees/PhillyPoacher.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS_Milestone_1.Entrees
+{
+    /// <summary>
+    /// Interaction logic for PhillyPoacher.xaml
+    /// </summary>
+    public partial class PhillyPoacher : UserControl
+    {
+        public PhillyPoacher()
+        {
+            InitializeComponent();
+        }
+    }
+}
